ad71b311877129b886dc6968203975e39578cc4e
meeeejin <meeeeejin@naver.com>
2020-03-07 16:30:00 +0900

    Adjust the LSN gap (ad-hoc)

--- storage/innobase/buf/buf0flu.cc
+++ storage/innobase/buf/buf0flu.cc
@@ -1458,7 +1458,7 @@ ibool buf_flush_page(buf_pool_t *buf_pool, buf_page_t *bpage,
         lsn_t lsn_gap = bpage->oldest_modification - before_lsn;
 
         /* FIXME: Ad-hoc method */
-        if (40000000000 < lsn_gap && lsn_gap < 80000000000) {
+        if (40000000000 < lsn_gap && lsn_gap < 70000000000) {
             bpage->moved_to_nvdimm = true;
             srv_stats.nvdimm_pages_stored.inc();
         }
